52dcd7dcab6d949ab3b3181ccb45d6bd23508b1a
shaunkh <shaun.khundker@gmail.com>
2023-05-27 16:40:46 -0400

    hi

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use cosmwasm_zero_to_hero::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
