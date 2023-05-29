98daa9d8f5eab0ad9db8f6d6b3ba16d1e9f8f9f3
shaunkh <shaun.khundker@gmail.com>
2023-05-29 12:41:55 -0400

    instantiate

--- src/contract.rs
+++ src/contract.rs
@@ -1,25 +1,30 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
-// use cw2::set_contract_version;
+use cw2::set_contract_version;
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::{Config, CONFIG};
 
-/*
 // version info for migration info
-const CONTRACT_NAME: &str = "crates.io:cosmwasm-zero-to-hero";
+const CONTRACT_NAME: &str = "crates.io:cosmwasm-zero-to-hero:shaunkh";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
-*/
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
     _info: MessageInfo,
-    _msg: InstantiateMsg,
+    msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+    let validated_admin_address = deps.api.addr_validate(&msg.admin_address)?;
+    let config = Config {
+        admin_address: validated_admin_address,
+    };
+    CONFIG.save(deps.storage, &config)?;
+    Ok(Response::new().add_attribute("action", "instantiate"))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
